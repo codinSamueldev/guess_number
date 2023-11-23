0dc1f343f9f7cd77d52cdd8caa8ec6cadfc7fc08
Samuel Henao <codinsamueldev@gmail.com>
2023-11-23 16:10:03 -0500

    feat: func. Computer now can guess our secret num

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import random
 
-def guess(x):
+def user_guess(x):
     numberToGuess = random.randint(1, x)
     userInput = 0
-    
 
     while userInput != numberToGuess:
         userInput = int(input("\nType your lucky number: "))
@@ -15,5 +14,27 @@ def guess(x):
 
     print(f"\nCongrats! your lucky {userInput} number it is correct. ")
 
+def computer_guess(y):
+    print("\nDear user, please think of a number and do not tell anyone. I am going to guess it.")
+    low = 1
+    high = y
+    i = ""
+    guessTimes = 0
+
+    while i != 'r':
+        computer_guess = random.randint(low, high)
+        i = input(f"\nDear user, my lucky number is {computer_guess}, type: \nf). If I am far away\nc). If I am close\nr). If I guessed right.\n-> ").lower()
+        if i == 'f':
+            print("Ok I will try again.")
+            guessTimes += 1
+        elif i == 'c':
+            print("Ok, that it is good to know. I will try again.")
+            guessTimes += 1
+
+    print(f"Dude, took me {guessTimes} tries. Anyways, it was fun!")
+
+
+
 if __name__ == '__main__':
-    guess(10)
+    #user_guess(10)
+    computer_guess(8)
